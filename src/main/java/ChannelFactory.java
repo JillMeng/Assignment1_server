0d2654e8b890db28d7c154db1fb9091e5acfdeda
JillMeng <76186275+JillMeng@users.noreply.github.com>
2022-03-15 00:05:00 -0700

    Added message broker RabbitMQ with Sample Send/Recv code

---
+++ src/main/java/ChannelFactory.java
@@ -0,0 +1,47 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import org.apache.commons.pool2.BasePooledObjectFactory;
+import org.apache.commons.pool2.DestroyMode;
+import org.apache.commons.pool2.PooledObject;
+import org.apache.commons.pool2.impl.DefaultPooledObject;
+
+import java.io.IOException;
+
+public class ChannelFactory
+        extends BasePooledObjectFactory<Channel> {
+
+    private Connection connection;
+
+    public ChannelFactory(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public Channel create() throws IOException {
+        return connection.createChannel();
+    }
+
+    /**
+     * Use the default PooledObject implementation.
+     */
+    @Override
+    public PooledObject<Channel> wrap(Channel channel) {
+        return new DefaultPooledObject<Channel>(channel);
+    }
+//
+//    /**
+//     * When an object is returned to the pool, clear the buffer.
+//     */
+//    @Override
+//    public void passivateObject(PooledObject<Channel> pooledObject) {
+//        pooledObject.getObject().setLength(0);
+//    }
+
+    @Override
+    public void destroyObject(PooledObject<Channel> p, DestroyMode destroyMode) throws Exception {
+        super.destroyObject(p, destroyMode);
+    }
+
+    // for all other methods, the no-op implementation
+    // in BasePooledObjectFactory will suffice
+}
